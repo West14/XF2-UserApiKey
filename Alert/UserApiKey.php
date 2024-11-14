04bed6083d17cfa2c9ecc7adb5a4d842f7c9e754
West <30056636+West14@users.noreply.github.com>
2024-11-14 17:59:40 +0200

    fix: fetch single entity for UserAlert content
    TypeError: Argument 1 passed to XF\Alert\AbstractHandler::canViewContent() must be an instance of XF\Mvc\Entity\Entity, instance of XF\Mvc\Entity\ArrayCollection given

--- Alert/UserApiKey.php
+++ Alert/UserApiKey.php
@@ -11,11 +11,17 @@ class UserApiKey extends AbstractHandler
         return ['expiration'];
     }
 
+    /**
+     * @param $id
+     * @return \XF\Mvc\Entity\ArrayCollection|\XF\Mvc\Entity\Entity|null
+     *
+     * we need to override this due to composite key usage
+     */
     public function getContent($id)
     {
         return \XF::finder('West\UserApiKey:UserApiKey')
             ->where('api_key_id', $id)
             ->keyedBy('api_key_id')
-            ->fetch();
+            ->fetchOne();
     }
 }
